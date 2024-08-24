6d6ef93644a682a86efaf043849ad94b3ee4d2ff
Meron <meronmuleta25@gmail.com>
2024-08-24 16:12:44 -0700

    modified

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -49,13 +49,13 @@ def is_member(user):
     return user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
-def librarian_view(request):
+def Librarian(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
-def member_view(request):
+def Member(request):
     return render(request, 'relationship_app/member_view.html')
