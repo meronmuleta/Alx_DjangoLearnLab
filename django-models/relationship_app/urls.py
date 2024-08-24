bd63b1eb2d0af79cd9a4366c691eace82c4d6ec4
Meron <meronmuleta25@gmail.com>
2024-08-24 15:28:14 -0700

    modified

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import SignUpView, Login, Logout
 urlpatterns = [
     path('views/',list_books),
     path('<int:pk>/',views.LibraryDetailView.as_view(),name="library_detail"),
-    path('register/', SignUpView.as_view(), name="register"),
-    path('login/', Login.as_view(), name="login"),
-    path('logout/',Logout.as_view(), name="logout"),
+    path('register/', views.register.as_view(), name="register"),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name="login"),
+    path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html'), name="logout"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,7 +14,7 @@ from django.contrib.auth import login, logout
 class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = "register.html"
+    template_name = "relationship_app/register.html"
 
 
 
