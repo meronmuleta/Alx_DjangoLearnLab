f5eefbcb91727e8a68cd256db21e3c44bbde988c
Meron <meronmuleta25@gmail.com>
2024-09-08 06:32:37 -0700

    m

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -5,6 +5,6 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author,related_name='books',on_delete=models.CASCADE)
-    published_date = models.DateField()
+    publication_year = models.IntegerField()
 
 # Create your models here.
