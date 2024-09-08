86e829911a124695121681728672fed181dfc470
Meron <meronmuleta25@gmail.com>
2024-09-08 01:17:39 -0700

    added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]  # Use token authentication
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]  # Apply permissions
