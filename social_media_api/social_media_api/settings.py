2071712f8270e14356a070d7c3604348a28736fa
Meron Diriba <itutuye@gmail.com>
2024-09-23 00:20:01 +0300

    modify

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -84,10 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_db_password',
-        'HOST': 'your_db_host',
-        'PORT': 'your_db_port',
+        'USER': 'postgres',
+        'PASSWORD': 'GkXySblgvQMPoasbGYSzmqlXcoqbvxei',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '8000',
     }
 }
 
