54cb8a5c378e8225f450f92c367f4099a8c1ffc2
Meron Diriba <itutuye@gmail.com>
2024-09-22 12:31:05 +0300

    user follows and feed functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserFollowView, UserUnfollowView
+from .views import RegisterView, LoginView, UserFollowView, UserUnfollowView, UserListView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('users/', UserListView.as_view(), name='user-list'),
     path('users/<int:user_id>/follow/', UserFollowView.as_view(), name='follow-user'),
     path('users/<int:user_id>/unfollow/', UserUnfollowView.as_view(), name='unfollow-user'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -37,6 +37,11 @@ class LoginView(APIView):
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
+class UserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
 class UserFollowView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
