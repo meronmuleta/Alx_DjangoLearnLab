ead763bcae06e5d13ed8bb7e4139fab69e192eb2
Meron Diriba <itutuye@gmail.com>
2024-09-22 12:36:14 +0300

    user follows and feed functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import PostViewSet, CommentViewSet, FeedView
+from .views import PostViewSet, CommentViewSet, UserFeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -9,5 +9,5 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('feed/', FeedView.as_view(), name='user-feed'),
+    path('feed/', UserFeedView.as_view(), name='user-feed'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions,generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -24,12 +23,11 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)  # Assign the comment author to the logged-in user
-class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+class UserFeedView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        followed_users = user.following.all()  # Get all users the current user is following
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
